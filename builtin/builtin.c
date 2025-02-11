a871df729a1ef45e100bf14f8d222875950fd282
fbartole <thekingpipp8@gmail.com>
2025-02-11 15:16:17 +0100

    exit status fix

--- builtin/builtin.c
+++ builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: barto <barto@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 11:42:25 by barto             #+#    #+#             */
-/*   Updated: 2025/02/11 14:57:09 by barto            ###   ########.fr       */
+/*   Updated: 2025/02/11 15:12:09 by barto            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,20 +109,24 @@ int	ft_exit(t_minishell *shell, char **args)
 	int	status;
 
 	ft_putendl_fd("exit", 2);
-	if (args[1])
+	if (!args[1])
 	{
-		if (!is_numeric(args[1]))
-		{
-			ft_putstr_fd("exit: ", 2);
-			ft_putstr_fd(args[1], 2);
-			ft_putendl_fd(": numeric argument required", 2);
-			shell->exit_status = 2;
-			exit(2);
-		}
-		status = ft_atoi(args[1]);
-	}
-	else
 		status = shell->exit_status;
-	shell->exit_status = status & 0xFF;
-	exit(shell->exit_status);
+		exit(status);
+	}
+	if (!is_numeric(args[1]))
+	{
+		ft_putstr_fd("exit: ", 2);
+		ft_putstr_fd(args[1], 2);
+		ft_putendl_fd(": numeric argument required", 2);
+		exit(2);
+	}
+	if (args[2])
+	{
+		ft_putendl_fd("exit: too many arguments", 2);
+		shell->exit_status = 1;
+		return (1);
+	}
+	status = ft_atoi(args[1]);
+	exit(status & 0xFF);
 }
